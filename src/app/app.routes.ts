c52779bb8b0d06f41d5fa8639848d169b038c3f0
Elies Martos <eliesmr4@gmail.com>
2017-09-04 19:17:57 +0200

    Página de producto y ruta lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ import {
 const APP_ROUTES: Routes = [
   { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'item', component: ItemComponent },
+  { path: 'item/:id', component: ItemComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -18,9 +18,12 @@ export class ProductosService {
        this.http.get('https://paginaweb-8cdcd.firebaseio.com/productos_idx.json')
         .subscribe (res =>{
 
-          console.log(res.json());
-          this.cargando = false;
-          this.productos = res.json();
+          //console.log(res.json());
+
+            this.cargando = false;
+            this.productos = res.json();
+
+
 
         })
 
